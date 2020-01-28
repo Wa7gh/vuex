1d35ad18c02f4cc52e8dcc6a5f47443fdff7a13c
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-01-28 10:00:09 +0300

    done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="home">
-    <button class="btn btn-praimery"> Add new Per </button>
-    <b-form @submit="handleSubmit"  v-if="showPet">
+    {{ getAllCats.length }}
+    {{ animalsCount }}
+    <button @click="toggelPetForm" class="btn btn-primary"> Add new Per </button>
+    <b-form @submit.prevent="handleSubmit"  v-if="showPet">
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
         <b-form-input
           id="input-2"
-          v-model="form.name"
+          v-model="formData.name"
           required
           placeholder="Enter name"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
         <b-form-select
           id="input-3"
-          v-model="form.food"
-          :options="foods"
+          v-model="formData.species"
+          :options="['cats','dogs']"
           required
         ></b-form-select>
       </b-form-group>
 
-      <b-form-group id="input-group-4">
-        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
+     <b-form-group id="input-group-2" label="Pet's Age:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="number"
+          v-model="formData.age"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
       </b-form-group>
 
       <b-button type="submit" variant="primary">Submit</b-button>
@@ -36,20 +41,48 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'home',
   data () {
     return {
-      showPet: false
+      showPet: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
     }
   },
+  computed: {
+    ...mapGetters([
+      'animalsCount',
+      'getAllCats'
+    ])
+  },
   methods: {
+    ...mapActions([
+      'addPet'
+    ]),
     toggelPetForm () {
       this.showPet = !this.showPet
     },
     handleSubmit () {
-
+      const { species, name, age } = this.formData
+      const payLoad = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payLoad)
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
     }
   }
 }
